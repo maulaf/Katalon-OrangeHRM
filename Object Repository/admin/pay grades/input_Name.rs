44347590c1ca462eb3aa894f41804c875b06a3a7
fetty athiyatul maula <fetty.maula98@gmail.com>
2024-12-13 16:34:36 +0700

    add TC.001 - 003 pay grades

---
+++ Object Repository/admin/pay grades/input_Name.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Name</name>
+   <tag></tag>
+   <elementGuidId>593421eb-465f-47b5-841c-25b0f5e6936d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//label[.='Name']/parent::div/following-sibling::*/input</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/admin/pay grades/ul_Pay Grades.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ul_Pay Grades</name>
+   <tag></tag>
+   <elementGuidId>5cfc55b6-f1e6-4f73-9d2a-b48d736b9f28</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@role='menuitem' and text()='Pay Grades']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
